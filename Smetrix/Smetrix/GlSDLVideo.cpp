56ef8722bed811d29bf49867ce17ee8f682854c4
AtoTheB <atb@engangilivet.se>
2009-07-12 11:18:36 +0200

    fixed all current compile errors and warnings

--- Smetrix/Smetrix/GlSDLVideo.cpp
+++ Smetrix/Smetrix/GlSDLVideo.cpp
@@ -1,6 +1,7 @@
 #include"GlSDLVideo.h"
+#include"Gameinfo.h"
 
-int gameinfo::initSdl(){
+int glsdlvideo::initSdl(){
 	
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
@@ -10,8 +11,8 @@ int gameinfo::initSdl(){
 	SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE,16 );
 	SDL_GL_SetAttribute( SDL_GL_ACCUM_RED_SIZE,5 );
 	SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER,1 );
-    SDL_SetVideoMode( 120, 220, 0, SDL_OPENGL | SDL_HWSURFACE | SDL_NOFRAME );
-    glViewport(0, 0, 120, 220);
+    SDL_SetVideoMode( m_nScreenwidth, m_nScreenheight, 0, SDL_OPENGL | SDL_HWSURFACE | SDL_NOFRAME );
+    glViewport(0, 0, m_nScreenwidth, m_nScreenheight);
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f );
     glClearDepth( 1.0 );
     glDepthFunc(GL_LESS);
--- Smetrix/Smetrix/GlSDLVideo.h
+++ Smetrix/Smetrix/GlSDLVideo.h
@@ -1,6 +1,7 @@
 #ifndef GLSDLVIDEO_H
 #define GLSDLVIDEO_H
 
+#include<windows.h>
 #include<SDL.h>
 #include<gl\gl.h>
 
@@ -8,6 +9,7 @@ class glsdlvideo{
 	SDL_Surface *screen;
 	const int m_nScreenwidth;
 	const int m_nScreenheight;
+
 
 public:
 	glsdlvideo() : m_nScreenheight(220), m_nScreenwidth(120)
