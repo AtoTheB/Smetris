7c31afe05a1282d02e7edb9c7319e8050a6fb0a2
AtoTheB <atb@engangilivet.se>
2009-07-15 21:08:48 +0200

    Added some gamestatus code with enums, dont know if it will be usefukk though.

--- Smetrix/Smetrix/Gameinfo.h
+++ Smetrix/Smetrix/Gameinfo.h
@@ -16,17 +16,28 @@ class gameinfo : public SDLInput{
 	// Nro 4x combos.
 	int m_nSmetrix;
 	//Return value from input i stored in this.
+	enum e_gameStatus : int{
+		UserExit = -1,
+		Running,
+		Paused,
+		Starting,
+		Exiting, 
+		Error
+	};
+
 	
 public:
 	gameinfo() : m_nPoints(0), m_nLevel(0), m_nRows(0), m_nSmetrix(0)
 	{
 	}
 	int gameLoop();
-
+	
+	//Dont know if i will use this, but added for remembering the way to do it.
+	e_gameStatus getGameStatus(){
+		if(1)
+		return Running;
+	}
 
 };
-
-
-
 
 #endif
