8189d4987932c15ad8fba0ebfb174719dbf21454
AtoTheB <atb@engangilivet.se>
2009-07-19 19:25:47 +0200

    small changes

--- Smetrix/Smetrix/Primitives.h
+++ Smetrix/Smetrix/Primitives.h
@@ -8,18 +8,23 @@
 class primitives : texture{
 //VARIABLES
 private:
+	
 	//What kind of smetrix cube, fix own class for this one to not screw up my fine primitive class.
 	int m_nType;
+	
 	//size of each cube side in pixels.;
 	//Gameplan height is 20*size pixels, gameplan width is 10*size pixels. 
 	double m_nSize;
+	
 	//Position, maybe fix so all corners get own x y pos, like this: 
 	/*
 	in constructor like &corners = new int [corners];
 	This will make draw more flexible.. Maybe just overload the constructor, so i can choose wich way to create a primitive.
+	LIMIT SO ONLY CAN DRAW primitives with 3 or 4 corners. 
 	*/
 	double m_nX;
 	double m_nY;
+	
 	//Number of corners.
 	int m_nCorners;
 	//Rotation 
@@ -37,7 +42,7 @@ protected:
 private:
 
 public:
-	primitives() : m_nType(0), m_nSize(20), m_dRotation(0)
+	primitives() : m_nType(0), m_nSize(20), m_dRotation(0), m_dDepth(0)
 	{
 	}
 	//Draw the primitive to m_SDLSurface
